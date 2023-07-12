ab8844ba528f92c47083382ce09657792bb94a01
Chams <tallbachir80@gmail.com>
2023-07-12 01:39:04 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,39 @@
+let input = document.querySelector("#input");
+let form = document.querySelector("form");
+let entrer = document.querySelector("#entrer");
+let tableau = [];
+
+
+entrer.addEventListener("click" , (e)=>{
+        e.preventDefault();
+        if(input.value === "" || isNaN(input.value)){
+            document.querySelector(".msg").innerHTML = "veuillez saisir un nombre"
+        }
+        tableau.push(parseFloat(input.value));
+        console.log(tableau);
+        input.value = "";
+});
+form.addEventListener("submit" , (e) =>{
+        e.preventDefault();
+        calculMoyenne(tableau);
+});
+function calculMoyenne(tab){
+        if (tab.length === 0) {
+            console.log("Le tableau est vide");
+            return;
+        }
+        let total = tab.reduce((accumulator , element) => {
+            accumulator = accumulator + element;
+            return accumulator;
+        },0)
+        const moyenne = total/tab.length ;
+        console.log(total);
+        console.log(moyenne);
+        let card = document.querySelector(".card");
+        card.classList.remove("d-none");
+        card.classList.add("d-block");
+        card.innerHTML =`la moyenne est : ${moyenne} `;
+}
+
+
+
