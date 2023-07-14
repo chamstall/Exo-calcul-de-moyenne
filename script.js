2ccc21e4b8313f8fb09371e4c43e83a73101b388
Chams <tallbachir80@gmail.com>
2023-07-14 00:01:35 +0000

    modif

--- script.js
+++ script.js
@@ -3,23 +3,22 @@ let form = document.querySelector("form");
 let entrer = document.querySelector("#entrer");
 let tableau = [];
 
-
 entrer.addEventListener("click" , (e)=>{
         e.preventDefault();
         if(input.value === "" || isNaN(input.value)){
             document.querySelector(".msg").innerHTML = "veuillez saisir un nombre"
         }
-        tableau.push(parseFloat(input.value));
+        else{
+            tableau.push(parseFloat(input.value));
+            document.querySelector(".msg").innerHTML = "";
+        }
+        input.value = "";
         console.log(tableau);
 });
 form.addEventListener("submit" , (e) =>{
         e.preventDefault();
-        if(input.value === "" || isNaN(input.value)){
-            console.log("input vide");
-        }
-        else{
-            calculMoyenne(tableau);
-        }
+        calculMoyenne(tableau);
+        tableau = [];
 });
 function calculMoyenne(tab){
         if (tab.length === 0) {
@@ -31,8 +30,7 @@ function calculMoyenne(tab){
             return accumulator;
         },0)
         const moyenne = total/tab.length ;
-        console.log(total);
-        console.log(moyenne);
+        console.log("la moyenne est :" + "" + moyenne);
         let card = document.querySelector(".card");
         card.classList.remove("d-none");
         card.classList.add("d-block");
