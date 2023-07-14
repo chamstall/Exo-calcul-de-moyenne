3762f5060461135c88f6928de040d5bf0792a69e
Chams <tallbachir80@gmail.com>
2023-07-14 13:11:32 +0000

    ajouts de l'affichage des nombres saisis par l'utilisateur

--- script.js
+++ script.js
@@ -3,42 +3,42 @@ let form = document.querySelector("form");
 let entrer = document.querySelector("#entrer");
 let tableau = [];
 
-entrer.addEventListener("click" , (e)=>{
-        e.preventDefault();
-        if(input.value === "" || isNaN(input.value)){
-            document.querySelector(".msg").innerHTML = "veuillez saisir un nombre"
-        }
-        else{
-            tableau.push(parseFloat(input.value));
-            document.querySelector(".msg").innerHTML = "";
-        }
-        input.value = "";
-        console.log(tableau);
-});
-form.addEventListener("submit" , (e) =>{
-        e.preventDefault();
-        calculMoyenne(tableau);
-        tableau = [];
-});
-function calculMoyenne(tab){
-        if (tab.length === 0) {
-            console.log("Le tableau est vide");
-            return;
-        }
-        let total = tab.reduce((accumulator , element) => {
-            accumulator = accumulator + element;
-            return accumulator;
-        },0)
-        const moyenne = total/tab.length ;
-        console.log("la moyenne est :" + "" + moyenne);
+entrer.addEventListener("click", (e) => {
+    e.preventDefault();
+    if (input.value === "" || isNaN(input.value)) {
+        document.querySelector(".msg").innerHTML = "veuillez saisir un nombre"
+    }
+    else {
+        tableau.push(parseFloat(input.value));
+        document.querySelector(".msg").innerHTML = "";
         let firstCard = document.querySelector(".firstcard");
         firstCard.classList.remove("d-none");
         firstCard.classList.add("d-block");
-        firstCard.innerHTML = `Les nombres que vous avez saisis sont : ${tableau}`
-        let secondCard = document.querySelector(".secondcard");
-        secondCard.classList.remove("d-none");
-        secondCard.classList.add("d-block");
-        secondCard.innerHTML =` la moyenne des nombres saisis est : ${moyenne} `;
+        firstCard.innerHTML = `nombre(s) saisi(s) : ${tableau}`
+    }
+    input.value = "";
+    console.log(tableau);
+});
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    calculMoyenne(tableau);
+    tableau = [];
+});
+function calculMoyenne(tab) {
+    if (tab.length === 0) {
+        console.log("Le tableau est vide");
+        return;
+    }
+    let total = tab.reduce((accumulator, element) => {
+        accumulator = accumulator + element;
+        return accumulator;
+    }, 0)
+    const moyenne = total / tab.length;
+    console.log("la moyenne est :" + "" + moyenne);
+    let secondCard = document.querySelector(".secondcard");
+    secondCard.classList.remove("d-none");
+    secondCard.classList.add("d-block");
+    secondCard.innerHTML = ` la moyenne des nombres saisis est : ${moyenne} `;
 }
 
 
